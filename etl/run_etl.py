a8007c2c86b58468cf3d90177d099b0342e61f0b
alan garcia <alan.josue.garcia.rdz@gmail.com>
2025-11-13 18:03:31 -0600

    Add initial ETL module and extraction components; update run_etl.py to include data extraction functions from Banxico, Investing.com, and Yahoo Finance; comment out transformation and loading steps for future implementation.

--- etl/run_etl.py
+++ etl/run_etl.py
@@ -1,10 +1,12 @@
+from .extract.banxico_extractor import extract_banxico
+from .extract.investiny_extractor import extract_investiny
+from .extract.yfinance_extractor import extract_yfinance
 
-
-def run_etl():
+if __name__ == "__main__":
     data_yf = extract_yfinance()
     data_inv = extract_investiny()
     data_banxico = extract_banxico()
     
-    df = transform_all(data_yf, data_inv, data_banxico)
-    load_to_postgres(df)
+    # df = transform_all(data_yf, data_inv, data_banxico)
+    # load_to_postgres(df)
 

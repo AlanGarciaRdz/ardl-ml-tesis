d5982f15851a7cf3d0e707bc41ad1923032e60ed
alan garcia <alan.josue.garcia.rdz@gmail.com>
2026-01-12 17:24:00 -0600

    remove double blank space

--- fe/src/components/layout/DashboardLayout.tsx
+++ fe/src/components/layout/DashboardLayout.tsx
@@ -12,7 +12,7 @@ export function DashboardLayout() {
         isMobileMenuOpen={isMobileMenuOpen} 
         onClose={() => setIsMobileMenuOpen(false)} 
       />
-      <div className="flex-1 flex flex-col overflow-hidden md:ml-64">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <Header onMobileMenuToggle={() => setIsMobileMenuOpen(true)} />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
           <Outlet />
